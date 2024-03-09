f04c6eb5b77e9cfb1dc229e74bcec03f7947015f
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-09 19:03:16 +0100

    Initial commit

---
+++ ExtraLandscapingTools.cs
@@ -0,0 +1,41 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using ExtraLib;
+using Unity.Entities;
+using Unity.Collections;
+using Game.Prefabs;
+
+namespace ExtraLandscapingTools
+{
+    public class ExtraLandscapingTools : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(ExtraLandscapingTools)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            EntityQueryDesc entityQueryDesc = new();
+            entityQueryDesc.All = [ComponentType.ReadOnly<TerraformingData>()];
+
+            ExtraLib.ExtraLib.AddOnEditEnity(new(OnEditEntities))
+
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+
+        private void OnEditEntities(Unity.Collections.NativeArray<Entity> entities)
+        {
+
+        }
+
+    }
+}
