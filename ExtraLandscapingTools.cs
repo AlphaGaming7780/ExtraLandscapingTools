309b36425b114066018554e23f2291c47a6cc3e1
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-09 19:14:32 +0100

    try 001

--- ExtraLandscapingTools.cs
+++ ExtraLandscapingTools.cs
@@ -6,36 +6,53 @@ using ExtraLib;
 using Unity.Entities;
 using Unity.Collections;
 using Game.Prefabs;
+using System;
+using ExtraLib.Helper;
 
 namespace ExtraLandscapingTools
 {
-    public class ExtraLandscapingTools : IMod
-    {
-        public static ILog log = LogManager.GetLogger($"{nameof(ExtraLandscapingTools)}").SetShowsErrorsInUI(false);
+	public class ExtraLandscapingTools : IMod
+	{
+		public static ILog log = LogManager.GetLogger($"{nameof(ExtraLandscapingTools)}").SetShowsErrorsInUI(false);
 
-        public void OnLoad(UpdateSystem updateSystem)
-        {
-            log.Info(nameof(OnLoad));
+		public void OnLoad(UpdateSystem updateSystem)
+		{
+			log.Info(nameof(OnLoad));
 
-            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
-                log.Info($"Current mod asset at {asset.path}");
+			if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+				log.Info($"Current mod asset at {asset.path}");
 
-            EntityQueryDesc entityQueryDesc = new();
-            entityQueryDesc.All = [ComponentType.ReadOnly<TerraformingData>()];
+			EntityQueryDesc entityQueryDesc = new()
+			{
+				All = [ComponentType.ReadOnly<TerraformingData>()]
+			};
 
-            ExtraLib.ExtraLib.AddOnEditEnity(new(OnEditEntities))
+			ExtraLib.ExtraLib.AddOnEditEnity(new(OnEditEntities, entityQueryDesc));
 
-        }
+		}
 
-        public void OnDispose()
-        {
-            log.Info(nameof(OnDispose));
-        }
+		public void OnDispose()
+		{
+			log.Info(nameof(OnDispose));
+		}
 
-        private void OnEditEntities(Unity.Collections.NativeArray<Entity> entities)
-        {
+		public void OnEditEntities(NativeArray<Entity> entities)
+		{   
+			foreach(Entity entity in entities) {
+				if(ExtraLib.ExtraLib.m_PrefabSystem.TryGetPrefab<TerraformingPrefab>(entity, out TerraformingPrefab prefab)) {
+					var TerraformingUI = prefab.GetComponent<UIObject>();
+					if (TerraformingUI == null)
+					{
+						TerraformingUI = prefab.AddComponent<UIObject>();
+						TerraformingUI.active = true;
+						TerraformingUI.m_IsDebugObject = false;
+						TerraformingUI.m_Icon = "Media/Game/Icons/LotTool.svg";
+						TerraformingUI.m_Priority = 1;
+					}
+					TerraformingUI.m_Group = PrefabsHelper.GetExistingToolCategory(prefab, "Terraforming") ?? TerraformingUI.m_Group;
+				}
+			}
+		}
 
-        }
-
-    }
+	}
 }
